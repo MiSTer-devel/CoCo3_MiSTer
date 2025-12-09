0cc97c86d96d2f2cc6df13f046fc124ef9d129c9
jackyangantelope <jack@retroremake.co>
2025-12-09 21:57:03 +0800

    Update sys (#17)

--- CoCo3.sv
+++ CoCo3.sv
@@ -43,6 +43,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -180,6 +182,8 @@ assign LED_POWER = 0;
 
 assign VGA_SCALER = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 wire [1:0] ar = status[9:8];
 
